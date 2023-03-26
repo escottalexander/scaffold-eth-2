88351c625f81fa6d84f025c82024afeeaf4c9c3c
escottalexander@gmail.com <escottalexander@gmail.com>
2023-03-26 15:59:32 -0400

    main functionality complete

--- packages/hardhat/contracts/CollateralizedExchange.sol
+++ packages/hardhat/contracts/CollateralizedExchange.sol
@@ -3,14 +3,15 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
+ * Enables two parties to exchange anything by collateralizing each side to safe guard against discoordination 
  */
-contract YourContract {
+contract CollateralizedExchange {
     using SafeERC20 for IERC20;
+    using SafeMath for uint;
+
     // Events
     event ItemListed(bytes32 indexed hash, address poster, uint index, uint price, address tokenContract);
     event ItemPriceUpdated(bytes32 indexed hash, address poster, uint index, uint newPrice);
@@ -34,71 +35,75 @@ contract YourContract {
     
     // State Variables
     mapping(address => mapping(uint => ItemListing)) itemListings;
-    mapping(address => uint) latestIndex; // TODO: Can't this be replaced by using an array? What am I missing?
+    mapping(address => uint) latestIndex;
     mapping(address => mapping(address => uint)) openCollateral;
     mapping(address => mapping(address => uint)) lockedCollateral;
 
     function checkCollateral(address checkAddress, uint amountRequired, address tokenContract) public view returns (bool hasEnough, uint amountUnder) {
         uint currentOpenCollateral = openCollateral[checkAddress][tokenContract];
-        bool hasEnough = currentOpenCollateral >= amountRequired;
-        uint amountUnder = hasEnough ? 0 : amountRequired - currentOpenCollateral;
+        hasEnough = currentOpenCollateral >= amountRequired;
+        amountUnder = hasEnough ? 0 : SafeMath.sub(amountRequired, currentOpenCollateral);
         return (hasEnough, amountUnder);
     }
 
-    function addOpenCollateral(uint amount, address tokenContract) public {
-        if (token != address(0x0)) {
+    function addOpenCollateral(uint amount, address tokenContract) public payable {
+        if (tokenContract != address(0x0)) {
             IERC20 token = IERC20(tokenContract);
-            // TODO: Do we need to check allowance???
-            //require(token.allowance(msg.sender, address(this)) >= price, "Token allowance for this contract too low);
             token.safeTransferFrom(msg.sender, address(this), amount);
-            openCollateral[msg.sender][tokenContract] += amount;
+            openCollateral[msg.sender][tokenContract] = openCollateral[msg.sender][tokenContract].add(amount);
         } else {
             // Must be native asset
             require(msg.value >= amount, "Not enough collateral to cover price");
             (bool success,) = address(this).call{value: msg.value}("");
             require(success, "Failed to receive native asset");
-            openCollateral[msg.sender][address(0x0)] += msg.value;
+            openCollateral[msg.sender][address(0x0)] = openCollateral[msg.sender][address(0x0)].add(msg.value);
         }
     }
 
+    /* 
+    ** This function handles a scenario when a user already has some openCollateral
+    ** so they only need to transfer value that is equal to the difference between 
+    ** item price and existing OC amount. Then this function locks the proper amount 
+    ** required to be locked by the item they are purchasing
+    */
     function addLockedCollateral(uint amount, uint price, address tokenContract) internal {
-        if (token != address(0x0)) {
+        if (tokenContract != address(0x0)) {
             IERC20 token = IERC20(tokenContract);
-            // TODO: Do we need to check allowance???
-            //require(token.allowance(msg.sender, address(this)) >= price, "Token allowance for this contract too low);
             token.safeTransferFrom(msg.sender, address(this), amount);
-            openCollateral[msg.sender][tokenContract] -= price - amount;
-            lockedCollateral[msg.sender][tokenContract] += price;
+            lockCollateral(SafeMath.sub(price, amount), price, tokenContract);
         } else {
             // Must be native asset
             (bool success,) = address(this).call{value: msg.value}("");
             require(success, "Failed to receive native asset");
-            openCollateral[msg.sender][address(0x0)] -= price - amount;
-            lockedCollateral[msg.sender][address(0x0)] += price;
+            lockCollateral(SafeMath.sub(price, amount), price, address(0x0));
         }
     }
 
-    function lockCollateral(uint amount, address tokenContract) internal {
-        openCollateral[msg.sender][tokenContract] -= price;
-        lockedCollateral[msg.sender][tokenContract] += price;
+    /*
+    ** This function accounts for the scenario where a user may already have some openCollateral
+    ** hence different amounts may need to be added/removed from locked and open collateral.
+    */
+    function lockCollateral(uint openToSub, uint lockedToAdd, address tokenContract) internal {
+        openCollateral[msg.sender][tokenContract] = openCollateral[msg.sender][tokenContract].sub(openToSub);
+        lockedCollateral[msg.sender][tokenContract] = lockedCollateral[msg.sender][tokenContract].add(lockedToAdd);
     }
 
     function unlockCollateral(uint amount, address tokenContract) internal {
-        lockedCollateral[msg.sender][tokenContract] -= price;
-        openCollateral[msg.sender][tokenContract] += price;
+        lockedCollateral[msg.sender][tokenContract] = lockedCollateral[msg.sender][tokenContract].sub(amount);
+        openCollateral[msg.sender][tokenContract] = openCollateral[msg.sender][tokenContract].add(amount);
     }
 
     function checkAndAddLockedCollateral(uint price, address tokenContract) internal {
         // Check that collateral has been provided
-        (hasEnough, amountUnder) = checkCollateral(msg.sender, price, tokenContract);
-        if (tokenContract == address(0x0)){
-            // Revert is msg.value is not equal to the amount owed
+        (bool hasEnough, uint amountUnder) = checkCollateral(msg.sender, price, tokenContract);
+        if (tokenContract == address(0x0)) {
+            // Revert if msg.value is not equal to the amount owed
             require(amountUnder == msg.value, "Reverted because we received too much value");
         }
 
         if (hasEnough) {
-            //  Lock Collateral
-            lockCollateral(price, tokenContract);
+            // Lock Collateral
+            lockCollateral(price, price, tokenContract);
         } else {
             // Add locked collateral
             addLockedCollateral(amountUnder, price, tokenContract);
@@ -110,7 +115,7 @@ contract YourContract {
         checkAndAddLockedCollateral(price, tokenContract);
         // List item
         uint index = latestIndex[msg.sender];
-        ItemListing item = itemListings[msg.sender][index];
+        ItemListing storage item = itemListings[msg.sender][index];
             
         item.itemHash = itemHash;
         item.price = price;
@@ -122,17 +127,18 @@ contract YourContract {
     }
 
     function updateListingPrice(uint itemIndex, uint newPrice) public {
-        ItemListing item = itemListings[msg.sender][itemIndex];
+        ItemListing storage item = itemListings[msg.sender][itemIndex];
         require(item.itemHash != '', "You have no items at that index");
+        require(item.price != newPrice, "You must provide a new price");
         require(item.buyer == address(0x0), "Someone has initiated buying this item, you cannot update it");
 
         if (newPrice > item.price) {
             // Add the difference between existing price and new price
-            checkAndAddLockedCollateral(newPrice - item.price, item.token);
+            checkAndAddLockedCollateral(SafeMath.sub(newPrice, item.price), item.token);
             item.price = newPrice;
         } else if (newPrice < item.price) {
             // Unlock the difference between existing price and new price
-            unlockCollateral(item.price - newPrice, item.token);
+            unlockCollateral(SafeMath.sub(item.price, newPrice), item.token);
             item.price = newPrice;
         }
 
@@ -140,7 +146,7 @@ contract YourContract {
     }
 
     function cancelListing(uint itemIndex) public {
-        ItemListing item = itemListings[msg.sender][itemIndex];
+        ItemListing storage item = itemListings[msg.sender][itemIndex];
         require(item.itemHash != '', "You have no items at that index");
         require(item.buyer == address(0x0), "Someone has initiated buying this item, you cannot cancel it");
 
@@ -152,10 +158,10 @@ contract YourContract {
 
     function buyItem(address seller, uint itemIndex) public payable {
         require(seller != msg.sender, "You cannot buy your own item, try canceling instead");
-        ItemListing item = itemListings[seller][itemIndex];
+        ItemListing storage item = itemListings[seller][itemIndex];
         require(item.buyer == address(0x0) && !item.isCanceled, "Item is no longer available");
         // Check for collateral and add any remaining, adding 2x the amount since half is the payment and half is collateral
-        checkAndAddLockedCollateral(item.price * 2, item.token);
+        checkAndAddLockedCollateral(SafeMath.mul(item.price, 2), item.token);
         item.buyer = msg.sender;
         
         emit ItemBought(item.itemHash, seller, msg.sender, itemIndex);
@@ -163,16 +169,16 @@ contract YourContract {
 
     function cancelBuy(address seller, uint itemIndex) public {
         require(seller != msg.sender, "Use the cancelListing method if you want to cancel your own listing");
-        ItemListing item = itemListings[seller][itemIndex];
+        ItemListing storage item = itemListings[seller][itemIndex];
         require(item.buyer == msg.sender && !item.isSent, "Item has already been sent and can no longer be canceled");
         item.buyer = address(0x0);
-        unlockCollateral(item.price * 2, item.token);
+        unlockCollateral(SafeMath.mul(item.price, 2), item.token);
         
         emit ItemBuyCanceled(item.itemHash, seller, msg.sender, itemIndex);
     }
 
     function markItemSent(uint itemIndex) public {
-        ItemListing item = itemListings[msg.sender][itemIndex];
+        ItemListing storage item = itemListings[msg.sender][itemIndex];
         require(item.itemHash != '', "You have no items at that index");
         require(item.buyer != address(0x0), "Nobody has bought this item, you can't mark as sent");
 
@@ -182,35 +188,33 @@ contract YourContract {
     }
 
     function markItemReceived(address seller, uint itemIndex) public {
-        ItemListing item = itemListings[seller][itemIndex];
+        ItemListing storage item = itemListings[seller][itemIndex];
         require(item.buyer == msg.sender, "You are not the buyer");
         require(item.isReceived == false, "Item has already been marked as received");
         item.isReceived = true;
         // Remove locked collateral from seller
-        lockedCollateral[seller][item.token] -= item.price;
+        lockedCollateral[seller][item.token] = lockedCollateral[seller][item.token].sub(item.price);
         // Remove locked collateral from buyer (they locked 2x the item price)
-        lockedCollateral[msg.sender][item.token] -= item.price * 2;
+        lockedCollateral[msg.sender][item.token] = lockedCollateral[msg.sender][item.token].sub(SafeMath.mul(item.price, 2));
         // Add open collateral to seller (notice how we are adding 2x the price, this is where the funds are moving from buyer to seller)
-        openCollateral[seller][item.token] += item.price * 2;
+        openCollateral[seller][item.token] = openCollateral[seller][item.token].add(SafeMath.mul(item.price, 2));
         // Return initial collateral to buyer
-        openCollateral[msg.sender][item.token] += item.price;
+        openCollateral[msg.sender][item.token] = openCollateral[msg.sender][item.token].add(item.price);
 
         emit ItemReceived(item.itemHash, seller, itemIndex);
     }
 
     function withdrawFunds(uint amount, address tokenContract) public {
         require(openCollateral[msg.sender][tokenContract] >= amount, "You don't have that amount of funds available");
-        if (token != address(0x0)) {
+        if (tokenContract != address(0x0)) {
             IERC20 token = IERC20(tokenContract);
-            // TODO: Do we need to check allowance???
-            //require(token.allowance(msg.sender, address(this)) >= price, "Token allowance for this contract too low);
             token.safeTransferFrom(address(this), msg.sender, amount);
-            openCollateral[msg.sender][tokenContract] -= amount;
+            openCollateral[msg.sender][tokenContract] = openCollateral[msg.sender][tokenContract].sub(amount);
         } else {
             // Must be native asset
             (bool success,) = msg.sender.call{value: amount}("");
             require(success, "Failed to send native asset");
-            openCollateral[msg.sender][address(0x0)] -= amount;
+            openCollateral[msg.sender][address(0x0)] = openCollateral[msg.sender][address(0x0)].sub(amount);
         }
     }
 
@@ -218,71 +222,16 @@ contract YourContract {
         return openCollateral[addr][tokenContract];
     }
 
-
-    // update price function
-    // Use SafeMath?
-    // accept any token for payment - how could we do this? Would require the abilty for buyer and seller to use different collaterals which gets hairy but might be possible with Uniswap oracles
-
-    // State Variables
-    address public immutable owner;
-    string public greeting = "Building Unstoppable Apps!!!";
-    bool public premium = false;
-    uint256 public totalCounter = 0;
-    mapping(address => uint) public userGreetingCounter;
-
-    // Events: a way to emit log statements from smart contract that can be listened to by external parties
-    event GreetingChange(address greetingSetter, string newGreeting, bool premium, uint256 value);
-
-    // Constructor: Called once on contract deployment
-    // Check packages/hardhat/deploy/00_deploy_your_contract.ts
-    constructor(address _owner) {
-        owner = _owner;
+    function getItem(address seller, uint itemIndex) public view returns (ItemListing memory) {
+        return itemListings[seller][itemIndex];
     }
 
-    // Modifier: used to define a set of rules that must be met before or after a function is executed
-    // Check the withdraw() function
-    modifier isOwner() {
-        // msg.sender: predefined variable that represents address of the account that called the current function
-        require(msg.sender == owner, "Not the Owner");
-        _;
-    }
-
-    /**
-     * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-     *
-     * @param _newGreeting (string memory) - new greeting to save on the contract
-     */
-    function setGreeting(string memory _newGreeting) public payable {
-        // Print data to the hardhat chain console. Remove when deploying to a live network.
-        console.log("Setting new greeting '%s' from %s",  _newGreeting, msg.sender);
-
-        // Change state variables
-        greeting = _newGreeting;
-        totalCounter += 1;
-        userGreetingCounter[msg.sender] += 1;
-
-        // msg.value: built-in global variable that represents the amount of ether sent with the transaction
-        if (msg.value > 0) {
-            premium = true;
-        } else {
-            premium = false;
+    function getItems(address seller) public view returns (ItemListing[] memory) {
+        ItemListing[] memory items = new ItemListing[](latestIndex[seller]);
+        for (uint i; i < latestIndex[seller]; i ++) {
+            items[i] = itemListings[seller][i];
         }
-
-        // emit: keyword used to trigger an event
-        emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, 0);
+        return items;
     }
 
-    /**
-     * Function that allows the owner to withdraw all the Ether in the contract
-     * The function can only be called by the owner of the contract as defined by the isOwner modifier
-     */
-    function withdraw() isOwner public {
-        (bool success,) = owner.call{value: address(this).balance}("");
-        require(success, "Failed to send Ether");
-    }
-
-    /**
-     * Function that allows the contract to receive ETH
-     */
-    receive() external payable {}
 }
